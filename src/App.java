e933eb5e03bbb2af6ca71c482e2fd423dc2ea3f7
luukalindgren <luslin@utu.fi>
2024-03-07 12:37:40 +0200

    Made TODO list

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+//TODO: Storyboard with the following:
+// - class Note with Title, text and tags
+// - class Storyboard with a list of notes
+// - JSON file to store the notes
+// - class NoteManager to read and write the JSON file
+// - Add CLI to addNote(), removeNote(), listNotes()
+// - searchByTitle(), searchByText(), searchByTag()
+// - Add some if-statements as unit tests
+// - Add some memory management and ensure efficiency
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
