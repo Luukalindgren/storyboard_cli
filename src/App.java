6ce3a921a4c0b1f7eecdf8636f799a40bc6695e8
luukalindgren <luslin@utu.fi>
2024-03-08 10:04:30 +0200

    Update TODO

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
-//TODO: Storyboard with the following:
-// - class Note with Title, text and tags
-// - class Storyboard with a list of notes
-// - JSON file to store the notes
-// - class NoteManager to read and write the JSON file
-// - Add CLI to addNote(), removeNote(), listNotes()
-// - searchByTitle(), searchByText(), searchByTag()
-// - Add some if-statements as unit tests
-// - Add some memory management and ensure efficiency
+/** TODO: Storyboard with the following:
+* - class Note with Title, text and tags
+* - class Storyboard with a list of notes
+* - class NoteManager to decouple the Storyboard from the Note operations
+* - Add CLI to addNote(), removeNote(), listNotes()
+* - searchByTitle(), searchByText(), searchByTag()
+* - Add some if-statements as unit tests
+* - Add some memory management and ensure efficiency
+*/
 
 public class App {
     public static void main(String[] args) throws Exception {
